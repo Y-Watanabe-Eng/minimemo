c1751db0c1b98351097cab38233950389ec092d8
Y-Watanabe-Eng <tofu.menta@gmail.com>
2023-06-13 16:26:49 +0900

    up

--- minimemopj/settings.py
+++ minimemopj/settings.py
@@ -9,7 +9,7 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.fspath(__file__)))
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env.prod'))
 
 # SECURITY WARNING
 SECRET_KEY = env('SECRET_KEY')
